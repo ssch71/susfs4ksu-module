bf371fa415b61d3daffa78e61b5a0b243d563896
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-07-06 20:58:36 +0800

    add date to exported susfs logs gzip file, export to /sdcard/Download, fix export error by getting only the first pid of the zygote64

--- src/features/home/export-logs.js
+++ src/features/home/export-logs.js
@@ -5,17 +5,25 @@ import { config } from '../../core/constants.js';
 // send logs
 export function susfs_send_logs(){
 	const susfs_send_logs_btn = document.getElementById('susfs_send_logs');
+	// Create a timestamp for the export file name
+	var currentDate = new Date();
+	var formattedDate = currentDate.getFullYear() + '-' +
+		String(currentDate.getMonth() + 1).padStart(2, '0') + '-' +
+		String(currentDate.getDate()).padStart(2, '0') + '_' +
+		String(currentDate.getHours()).padStart(2, '0') + '-' +
+		String(currentDate.getMinutes()).padStart(2, '0') + '-' +
+		String(currentDate.getSeconds()).padStart(2, '0');
 	susfs_send_logs_btn.addEventListener('click', async function(event) {
 		try {
-		await run(`cat /proc/$(pidof zygote64)/mountinfo > /data/adb/ksu/susfs4ksu/zygote64_mountinfo.txt`);
-		await run(`cat /proc/$(pidof zygote64)/maps > /data/adb/ksu/susfs4ksu/zygote64_maps.txt`);
-		await run(`cat /proc/1/mountinfo > /data/adb/ksu/susfs4ksu/pid1_mountinfo.txt`);
-		await run(`cp /data/adb/ksu/log/dmesg.log /data/adb/ksu/susfs4ksu/dmesg.log`);
-		await run(`ksud module list > /data/adb/ksu/susfs4ksu/ksu_module_list.txt`);
-		await run(`dmesg | grep susfs > /data/adb/ksu/susfs4ksu/latest_dmesg_susfs.log`);
-		await run(`tar -C /data/adb/ksu/susfs4ksu/ -czvf /sdcard/susfs_logs.tar.gz .`);
-		toast("Logs saved to /sdcard/susfs_logs.tar.gz");
-		await run(`am start -a android.intent.action.SEND -t '*/*' -c android.intent.category.DEFAULT --eu android.intent.extra.STREAM 'file:///sdcard/susfs_logs.tar.gz'`);
+			await run(`cat /proc/$(pidof zygote64 | awk '{print $1}')/mountinfo > /data/adb/ksu/susfs4ksu/zygote64_mountinfo.txt`);
+			await run(`cat /proc/$(pidof zygote64 | awk '{print $1}')/maps > /data/adb/ksu/susfs4ksu/zygote64_maps.txt`);
+			await run(`cat /proc/1/mountinfo > /data/adb/ksu/susfs4ksu/pid1_mountinfo.txt`);
+			await run(`cp /data/adb/ksu/log/dmesg.log /data/adb/ksu/susfs4ksu/dmesg.log`);
+			await run(`ksud module list > /data/adb/ksu/susfs4ksu/ksu_module_list.txt`);
+			await run(`dmesg | grep susfs > /data/adb/ksu/susfs4ksu/latest_dmesg_susfs.log`);
+			await run(`tar -C /data/adb/ksu/susfs4ksu/ -czvf /sdcard/Download/susfs_logs_${formattedDate}.tar.gz .`);
+			toast("Logs saved to /sdcard/Download/susfs_logs_" + formattedDate + ".tar.gz");
+			await run(`am start -a android.intent.action.SEND -t '*/*' -c android.intent.category.DEFAULT --eu android.intent.extra.STREAM 'file:///sdcard/Download/susfs_logs_${formattedDate}.tar.gz'`);
 		} catch (error) {
 			toast("Failed to prepare logs: " + error.message);
 		}
